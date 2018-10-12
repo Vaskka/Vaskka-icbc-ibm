ef8fb280fc38a88e8bff9c99d6c1b29e97fc0d0d
Vaskka <1139851358@qq.com>
2018-10-12 21:00:05 +0800

    uwsgi conf

--- backend/icbc_ibm/icbc_ibm/settings.py
+++ backend/icbc_ibm/icbc_ibm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xe#x6pfml11%%j+qm8ipcp6&r3e5sq7k1&a85a2^jvf#+op+%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
